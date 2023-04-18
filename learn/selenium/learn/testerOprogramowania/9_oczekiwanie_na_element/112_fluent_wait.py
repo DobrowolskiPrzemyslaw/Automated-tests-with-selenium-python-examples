ca5bd09293f8135a92ab25f5bc9a48601b21c27e
DobrowolskiPrzemyslaw <dobrowolski.przemyslaw.1991@gmail.com>
2023-04-18 08:56:26 +0200

    Added new examples of waits

--- learn/selenium/learn/testerOprogramowania/9_oczekiwanie_na_element/112_fluent_wait.py
+++ learn/selenium/learn/testerOprogramowania/9_oczekiwanie_na_element/112_fluent_wait.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common import ElementNotVisibleException, ElementNotSelectableException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
@@ -7,7 +8,7 @@ driver = webdriver.Chrome()
 driver.get("C:\\Users\\User\\PycharmProjects\\LEARNETIC\\learn\\selenium\\resources\\Waits.html")
 driver.maximize_window()
 driver.find_element(By.ID, "clickOnMe").click()
-wait = WebDriverWait(driver, timeout=10)
+wait = WebDriverWait(driver, timeout=10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
 element = wait.until(expected_conditions.visibility_of_element_located((By.TAG_NAME,"p")))
 print(element.text)
 # input('Press enter to continue...')
--- learn/selenium/learn/testerOprogramowania/9_oczekiwanie_na_element/113_wlasnyWarunek_classa.py
+++ learn/selenium/learn/testerOprogramowania/9_oczekiwanie_na_element/113_wlasnyWarunek_classa.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+
+class WaitForListSize:
+    def __init__(self, locator):
+        self.locator = locator
+    def __call__(self, driver):
+        return driver.find_elements(By.XPATH, self.locator)
 
 driver = webdriver.Chrome()
 driver.get("C:\\Users\\User\\PycharmProjects\\LEARNETIC\\learn\\selenium\\resources\\Waits.html")
 driver.maximize_window()
 driver.find_element(By.ID, "clickOnMe").click()
 wait = WebDriverWait(driver, timeout=10)
-element = wait.until(expected_conditions.visibility_of_element_located((By.TAG_NAME,"p")))
-print(element.text)
+wait.until(WaitForListSize("//p"))
+print(driver.find_element(By.TAG_NAME,"p").text)
 # input('Press enter to continue...')
+
+
