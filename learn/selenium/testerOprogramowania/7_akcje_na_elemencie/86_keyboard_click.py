91e470d2956acafe9f8763f86dd1d4f30f146c9e
DobrowolskiPrzemyslaw <dobrowolski.przemyslaw.1991@gmail.com>
2023-04-20 10:01:59 +0200

    Refactor code

--- learn/selenium/testerOprogramowania/7_akcje_na_elemencie/86_keyboard_click.py
+++ learn/selenium/testerOprogramowania/7_akcje_na_elemencie/86_keyboard_click.py
@@ -11,5 +11,6 @@ driver.switch_to.alert.accept()
 click_me_button.click()
 driver.switch_to.alert.dismiss()
 driver.find_element(By.ID, "fname").send_keys("Wpisz cos")
+
 driver.find_element(By.ID, "password").send_keys("webdriver" + Keys.ENTER)
 input('Press enter to continue...')
--- learn/selenium/testerOprogramowania/7_akcje_na_elemencie/87_selecta.py
+++ learn/selenium/testerOprogramowania/7_akcje_na_elemencie/87_selecta.py
@@ -5,8 +5,10 @@ from selenium.webdriver.support.select import Select
 driver = webdriver.Chrome()
 driver.get("C:\\Users\\User\\PycharmProjects\\LEARNETIC\\learn\\selenium\\resources\\Test.html")
 driver.maximize_window()
-autoSelect =  Select(driver.find_element(By.TAG_NAME, "select"))
+
+autoSelect = Select(driver.find_element(By.TAG_NAME, "select"))
 autoSelect.select_by_visible_text("Volvo")
 autoSelect.select_by_value("saab")
 autoSelect.select_by_index(0)
+
 input('Press enter to continue...')
